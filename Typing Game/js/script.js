9d7b4ce74818d7dbd13732040d9923a62a7cd858
richards2058 <richard.535200058@stu.untar.ac.id>
2021-10-25 18:46:07 +0700

    ganti sistem

--- Typing Game/js/script.js
+++ Typing Game/js/script.js
@@ -6,15 +6,15 @@ window.addEventListener("load", waitStart);
 
 // Available Levels
 const levels = {
-  easy: 5,
-  medium: 3,
-  hard: 2,
+  easy: "easy",
+  medium: "medium",
+  hard: "hard",
 };
-
+const initialTime =  60;
 // To change level
 let currentLevel = levels.easy;
 
-let time = currentLevel;
+let time = initialTime;
 let score = 0;
 let isPlaying = true;
 let maxScore;
@@ -29,12 +29,133 @@ const timeDisplay = document.querySelector("#time");
 const message = document.querySelector("#message");
 const seconds = document.querySelector("#seconds");
 const highScoreElt = document.querySelector("#high-score");
+const difficulty = document.querySelector("#level");
 
 const easyBtn = document.querySelector("#easy");
 const mediumBtn = document.querySelector("#medium");
 const hardBtn = document.querySelector("#hard");
 
-const words = [
+const wordsEasy = [
+  "angular",
+  "magic",
+  "brew",
+  "while",
+  "throw",
+  "css",
+  "break",
+  "swing",
+  "echo",
+  "let",
+  "wall",
+  "laughter",
+  "hash",
+  "spinner",
+  "beer",
+  "ninja",
+  "javascript",
+  "master",
+  "program",
+  "coding",
+  "hero",
+  "learning",
+  "work",
+  "case",
+  "react",
+  "dragon",
+  "rush",
+  "api",
+  "init",
+  "motion",
+  "google",
+  "float",
+  "damn",
+  "block",
+  "ranking",
+  "nice",
+  "machine",
+  "perfect",
+  "deploy",
+  "terminal",
+  "array",
+  "vue",
+  "node",
+  "html",
+  "front",
+  "grid",
+  "stack",
+  "mac",
+  "console",
+  "ajax",
+  "heroku",
+  "loop",
+  "sql",
+  "php",
+  "data",
+  "npm",
+  "server",
+  "bash",
+];
+const wordsMed = [
+  "angular",
+  "magic",
+  "brew",
+  "while",
+  "throw",
+  "css",
+  "break",
+  "swing",
+  "echo",
+  "let",
+  "wall",
+  "laughter",
+  "hash",
+  "spinner",
+  "beer",
+  "ninja",
+  "javascript",
+  "master",
+  "program",
+  "coding",
+  "hero",
+  "learning",
+  "work",
+  "case",
+  "react",
+  "dragon",
+  "rush",
+  "api",
+  "init",
+  "motion",
+  "google",
+  "float",
+  "damn",
+  "block",
+  "ranking",
+  "nice",
+  "machine",
+  "perfect",
+  "deploy",
+  "terminal",
+  "array",
+  "vue",
+  "node",
+  "html",
+  "front",
+  "grid",
+  "stack",
+  "mac",
+  "console",
+  "ajax",
+  "heroku",
+  "loop",
+  "sql",
+  "php",
+  "data",
+  "npm",
+  "server",
+  "bash",
+];
+const wordsHard = [
   "angular",
   "magic",
   "brew",
@@ -102,6 +223,12 @@ $("#optionBtn").click( ()=>{
   menuSlideElt.classList.toggle("slideIn");
 });
 
+function showEndMessage(){
+  var $score = $("<h3/>").addClass("appended").html(`<h3>Score : ${score}</h3>`);
+  var $pesan = $("<h3/>").addClass("appended").html(`<h3>Good Job!</h3>`);
+  $('#main-view').append($score);
+  $('#main-view').append($pesan);
+}
 
 function sleep(ms) {
   return new Promise((resolve) => startCount = setTimeout(resolve, ms));
@@ -112,12 +239,17 @@ function setlevel(e) {
   if (e.target === easyBtn) {
     currentLevel = levels.easy;
   } else if (e.target === mediumBtn) {
-    currentLevel = levels.medium;
+    currentLevel =  levels.medium;
   } else if (e.target === hardBtn) {
     currentLevel = levels.hard;
   }
   console.log(currentLevel);
+  time = 0;
+  timeDisplay.innerHTML = time;
   menuSlideElt.classList.toggle("slideIn");
+  clearInterval(countDown);
+  wordInput.value = "";
+  $("#start").prop('disabled', false);
   waitStart();
 }
 
@@ -127,13 +259,17 @@ function waitStart() {
     currentWord.innerHTML = "Click start";
     message.innerHTML = "";
   }else{
-    currentWord.innerHTML = "Game Over";
+    currentWord.innerHTML = `Time's up `;
+    showEndMessage();
     message.innerHTML = "Click start to play again";
   }
-  seconds.innerHTML = currentLevel;
+  seconds.innerHTML = initialTime;
+  difficulty.innerHTML = currentLevel;
 
   //button start diklik
   $("#start").click(async () => {
+    $(":header").remove(".appended");
+    score = 0;
     wordInput.value = "";
     $("#start").prop('disabled', true);
     for (let i = 3; i > 0; i--) {
@@ -154,8 +290,8 @@ function waitStart() {
 // Initialize Game
 function init() {
   // Load word from array
-  showWord(words);
-  time = currentLevel;
+  showWord();
+  time = initialTime;
   timeDisplay.innerHTML = time;
 
   // Start matching on word input
@@ -174,8 +310,7 @@ function startMatch() {
   if (matchWords()) {
     // isPlaying = true;
     scoreDisplay.innerHTML = score;
-    time = currentLevel;
-    showWord(words);
+    showWord();
     
     wordInput.value = "";
     if (currentLevel === levels.easy) {
@@ -214,9 +349,15 @@ function matchWords() {
 
 // Pick and show random word
 function showWord(word) {
-  // Generate random array index
+  var words;
+  if (currentLevel === levels.easy) {
+    words = wordsEasy;
+  } else if (currentLevel === levels.medium) {
+    words = wordsMed;
+  } else if (currentLevel === levels.hard) {
+    words = wordsHard;
+  }
   const randIndex = Math.floor(Math.random() * words.length);
-  // Output random word
   currentWord.innerHTML = words[randIndex];
 }
 
@@ -236,7 +377,7 @@ function countdown() {
   } else if (time === 0) {
     // Game is over
     isPlaying = false;
-    score = 0;
+   
     clearInterval(countDown);
     wordInput.value = "";
     $("#start").prop('disabled', false);
