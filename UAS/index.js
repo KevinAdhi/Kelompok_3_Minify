3b82a150b474b8bd9011d339d10e37c2ff63db4a
richards2058 <richard.535200058@stu.untar.ac.id>
2021-11-03 20:06:44 +0700

    ganti jadi main layout

--- UAS/index.js
+++ UAS/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const layouts = require("express-ejs-layouts");
+
 const app = express();
 const PORT = 3000;
+
+// use layouts
+app.use(layouts);
+app.set("layout", "layouts/main.ejs");
+
+// place all styles block in the layout at the head
+app.set("layout extractStyles", true);
+// place all scripts block in the layout at the end
+app.set("layout extractScripts", true);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -10,9 +21,9 @@ const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
 app.get("/", (req, res) => {
-  res.render("pages/index");
+    res.render("pages/index");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server Berjalan di port ${PORT}`);
+    console.log(`Server Berjalan di port ${PORT}`);
 });
