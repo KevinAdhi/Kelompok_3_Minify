3df58f57fc28f4ae0d055f21af994bf83982a971
richards2058 <richard.535200058@stu.untar.ac.id>
2021-11-04 21:19:31 +0700

    tambah pages ejs

--- UAS/routes/index.js
+++ UAS/routes/index.js
@@ -3,25 +3,44 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("pages/index");
+    res.render("pages/index");
 });
 router.get("/homePage", (req, res) => {
-  res.render("pages/homePage");
+    res.render("pages/homePage");
 });
 router.get("/checkout", (req, res) => {
-  res.render("pages/checkout");
+    res.render("pages/checkout");
 });
 router.get("/payment", (req, res) => {
-  res.render("pages/payment");
+    res.render("pages/payment");
 });
 router.get("/myorder", (req, res) => {
-  res.render("pages/myorder");
+    res.render("pages/myorder");
 });
 router.get("/myOrderDetails", (req, res) => {
-  res.render("pages/myOrderDetails");
+    res.render("pages/myOrderDetails");
 });
 router.get("/message", (req, res) => {
-  res.render("pages/message");
+    res.render("pages/message");
+});
+router.get("/dashboard", (req, res) => {
+    res.render("pages/dashboard");
+});
+
+router.get("/chat-room", (req, res) => {
+    res.render("pages/chatRoom");
+});
+
+router.get("/profile", (req, res) => {
+    res.render("pages/profile");
+});
+
+router.get("/edit-profile", (req, res) => {
+    res.render("pages/editProfile");
+});
+
+router.get("/edit-product", (req, res) => {
+    res.render("pages/editProduct");
 });
 
 module.exports = router;
