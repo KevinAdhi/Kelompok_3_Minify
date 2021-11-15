06dacbc19bf94d768212b54944d5d4c72c842cdd
Kevin Adhi <kevinadhi21@gmail.com>
2021-11-15 13:38:39 +0700

    update

--- UAS/seed/seedScript.js
+++ UAS/seed/seedScript.js
@@ -14,33 +14,6 @@ mongoose.connect(
   }
 );
 
-const products = [
-  new Product({
-    imagePath:
-      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    name: "Test",
-    price: 100000,
-  }),
-  new Product({
-    imagePath:
-      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    name: "Test1",
-    price: 100000,
-  }),
-  new Product({
-    imagePath:
-      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    name: "Test2",
-    price: 100000,
-  }),
-  new Product({
-    imagePath:
-      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    name: "Test3",
-    price: 100000,
-  }),
-];
-
 var done = 0;
 for (var i = 0; i < products.length; i++) {
   products[i].save((err, res) => {
