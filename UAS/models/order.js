2e556dea2cd7bbacd3696f1b28001b6aa9f8f3ea
Hongi <hongi.535200082@stu.untar.ac.id>
2021-11-16 22:47:26 +0700

    disable models order

--- UAS/models/order.js
+++ UAS/models/order.js
@@ -1,30 +1,30 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const orderSchema = mongoose.Schema({
-  user: {
-    type: orderSchema.Types.ObjectId,
-    ref: "User",
-  },
-  cart: {
-    type: Object,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  imagePath: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  id: {
-    type: String,
-    required: true,
-  },
-});
+// const orderSchema = mongoose.Schema({
+//   user: {
+//     type: orderSchema.Types.ObjectId,
+//     ref: "User",
+//   },
+//   cart: {
+//     type: Object,
+//     required: true,
+//   },
+//   address: {
+//     type: String,
+//     required: true,
+//   },
+//   imagePath: {
+//     type: String,
+//     required: true,
+//   },
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   id: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-module.exports = mongoose.model("Order", orderSchema, "order");
+// module.exports = mongoose.model("Order", orderSchema, "order");
